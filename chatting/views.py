7efd863901a74e16ec86fe6d0f2c8db9caf806ef
ggengmo <rudah365dlf@gmail.com>
2023-11-24 15:37:07 +0900

    대화 기록 DB 저장

--- chatting/views.py
+++ chatting/views.py
@@ -1,10 +1,11 @@
-# chatbot > views.py
+# chatting/views.py
 
 from django.shortcuts import render
 from django.views import View
 from dotenv import load_dotenv
 from openai import OpenAI
 import os
+from .models import Conversation
 
 load_dotenv()
 client = OpenAI(
@@ -28,12 +29,17 @@ class ChatbotView(View):
             ]
             previous_conversations.append({"role": "user", "content": prompt})
 
-            model_engine = "gpt-3.5-turbo"  # 수정된 부분
-            completions = client.chat.completions.create(  # 수정된 부분
+            model_engine = "gpt-3.5-turbo"
+            completions = client.chat.completions.create(
                 model=model_engine,
-                messages=previous_conversations  # 수정된 부분
+                messages=previous_conversations
             )
             response = completions.choices[0].message.content
+
+            # 대화 기록 DB에 저장
+            conversation_db = Conversation(prompt=prompt, response=response)
+            conversation_db.save()
+
             conversation = {'prompt': prompt, 'response': response}
             # 대화 기록에 새로운 응답 추가
             session_conversations.append(conversation)
