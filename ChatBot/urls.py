9941d14715e33a821c6ff2d94fa3be1327f20226
ggengmo <rudah365dlf@gmail.com>
2023-12-01 04:11:47 +0900

    main 앱 추가

--- ChatBot/urls.py
+++ ChatBot/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('main.urls')),
     path('accounts/', include('accounts.urls')),
     path('chatting/', include('chatting.urls')),
 ]
